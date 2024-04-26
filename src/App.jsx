75f5d6d42bf58dfcda02ad8c2cc85a18df950d07
RIZQI NUR RIFA'I <84585203+rifai-rizqi3@users.noreply.github.com>
2024-04-26 09:22:17 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Rizqi Nur Rifai",
+  title: "DevOps Engineer",
+  email: "rifairizqi.hao@gmail.com",
+  gitHub: "rifai-rizqi3",
+  instagram: "otis.oreo12",
+  linkedIn: "rnrifai",
   medium: "",
-  twitter: "microsoft",
+  twitter: "rifai-rizqi3",
   youTube: "Code",
 };
 
